d37a2e23bc6f99bf682ddf987091b25896f7bc49
CarrieShort <carrie.r.phillips@gmail.com>
2016-02-15 19:13:00 -0800

    functions outside of object, listify working great, iterating through object, need to change driver messaging

--- app.js
+++ app.js
@@ -48,17 +48,72 @@ var deliveries = example.generateRandom(example[x][3],example[x][4]);
 
 */
 
-
-var listFodder = ['things', 45, 'blue bears', 'beagles', 99];
-var placeForMahList = document.getElementById('ballard');
-function listify(listed) {
-  for (var i=0; i < listed.length; i++) {
-
-    //build an li element
-    var liEl = document.createElement('li');
-    liEl.textContent = listed[i];
-    placeForMahList.appendChild(liEl);
-  }
+var example = {
+  t1: ['8:00am', 0, 4,0,4],
+  t2: ['9:00am', 0, 4,0,4],
+  t3: ['10:00am', 0, 4,0,4],
+  t4: ['11:00am', 0, 7,0,4],
+  t5: ['12:00am', 0, 7,0,4],
+  t6: ['1:00pm', 0, 7,0,4],
+  t7: ['2:00pm', 2, 15,1,4],
+  t8: ['3:00pm', 2, 15,1,4],
+  t9: ['4:00pm', 2, 15,1,4],
+  t10: ['5:00pm', 15, 35,3,8],
+  t11: ['6:00pm', 15, 35,3,8],
+  t12: ['7:00pm', 15, 35,3,8],
+  t13: ['8:00pm', 12, 31,5,12],
+  t14: ['9:00pm', 12, 31,5,12],
+  t15: ['10:00pm', 12, 31,5,12],
+  t16: ['11:00pm', 5, 20,6,11],
+  t17: ['11:00pm', 5, 20,6,11],
+  t18: ['11:00pm', 5, 20,6,11]
 }
 
-listify(listFodder);
+function generateRandom(min,max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function calcDrivers(deliveries) {
+  if (deliveries % 3 === 0) {
+    var drivers = deliveries / 3;
+  } else {
+    var drivers = Math.floor(deliveries/3) + 1
+  }
+  return driversMsg;
+}
+
+
+
+function listify (objectVar, ulId) {
+  console.log(objectVar);
+  console.log(ulId);
+  var placeForMahList = document.getElementById(ulId);
+  var x;
+  for (x in objectVar) {
+    var hour = objectVar[x][0];
+    var pizzas = generateRandom(objectVar[x][1],objectVar[x][2]);
+    var numDeliveries = generateRandom(objectVar[x][3],objectVar[x][4]);
+    var numDrivers = calcDrivers(numDeliveries);
+    console.log('the time is: ' + hour);
+    console.log('number of pizzas are: ' + pizzas);
+    console.log('number of deliveries are : ' + numDeliveries);
+    console.log('number of recomended drivers are :' + numDrivers);
+    var liEl = document.createElement('li');
+    liEl.textContent = hour + ' ' + pizzas + ' pizzas, ' + numDeliveries + ' deliveries -- [ drivers recommended: ' + numDrivers + ' ]' ;
+    placeForMahList.appendChild(liEl);
+  }
+};
+listify(example,'ballard');
+
+// var listFodder = ['things', 45, 'blue bears', 'beagles', 99];
+// var placeForMahList = document.getElementById('ballard');
+// function listify(listed) {
+//   for (var i=0; i < listed.length; i++) {
+//
+//     //build an li element
+//     var liEl = document.createElement('li');
+//     liEl.textContent = listed[i];
+//     placeForMahList.appendChild(liEl);
+//   }
+// }
+//
+// listify(listFodder);
